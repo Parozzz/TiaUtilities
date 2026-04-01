6e9887061571f78dd596785acc98065a8a43f76c
Parozzz <paronetto.giacomo@gmail.com>
2026-04-01 19:36:48 +0200

    Dirty TIA V21 Addin
    A lot has changed and more needs to be changed on my side! Will be done after this branch is closed

---
+++ AddIn_V21/AddInProvider.cs
@@ -0,0 +1,22 @@
+﻿using Siemens.Engineering;
+using Siemens.Engineering.AddIn;
+using Siemens.Engineering.AddIn.Menu;
+using System.Collections.Generic;
+
+namespace AddIn_V21
+{
+    public sealed class AddInProvider : ProjectTreeAddInProvider
+    {
+        private readonly TiaPortal _tiaPortal;
+
+        public AddInProvider(TiaPortal tiaPortal)
+        {
+            _tiaPortal = tiaPortal;
+        }
+
+        protected override IEnumerable<ContextMenuAddIn> GetContextMenuAddIns()
+        {
+            yield return new AddIn_V21(_tiaPortal);
+        }
+    }
+}
